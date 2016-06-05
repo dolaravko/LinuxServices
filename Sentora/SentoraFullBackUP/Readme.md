7ee5f57f5bfcedef289f7b8152a83248519591fc
Slavko Stojadinovic <dolaravko@gmail.com>
2016-06-05 19:53:56 +0200

    Add Sentora Backup

---
+++ Sentora/SentoraFullBackUP/Readme.md
@@ -0,0 +1,18 @@
+# Sentora Full BackUP
+
+
+## Preparing Sentora server
+
+1. Install xtrabackup [percona xtrabackup](https://www.percona.com/doc/percona-xtrabackup/2.4/installation/apt_repo.html)<br>
+On Sentora server (Ubuntu):<br>
+```
+wget https://repo.percona.com/apt/percona-release_0.1-3.$(lsb_release -sc)_all.deb```
+```sudo dpkg -i percona-release_0.1-3.$(lsb_release -sc)_all.deb```
+```sudo apt-get update```
+```sudo apt-get install percona-xtrabackup-24```
+
+2. Create folder for backup on Sentora server<br>```mkdir  -p /home/backup```
+
+3. Create folder for mysql backup and script folder on Sentora server<br>```mkdir  -p /home/backup/mysql``` ```mkdir  -p /home/backup/script```
+4. Put script on folder<br>```vim /home/backup/script/sentoraFullBackup.sh```
+
---
+++ Sentora/SentoraFullBackUP/sentoraFullBackup.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+# 2016.05.10
+# create by $avko
+#
+# 	sentoraFullBackUP.sh
+
+# full day YYYY-MM-DD
+#DAY=`date +%Y-%m-%d`
+# day in week 
+DAY=`date '+%u'`
+
+
+# backup location on sentora server
+FOLDERBACKUP="/home/backup"
+
+# mysql root password
+ROOTPASSWORDFORMYSQL="yourMySqlRootPassword"
+
+
+# ip or host for backup server
+BACKUPSERVERIP="backup.server.com"
+# user for backup server
+BACKUPSERVERUSER="sentora"
+
+# location for backp on backup server
+BACKUPSERVERFOLDER="/home/sentora/backup/"
+
+# loacation of ssh private key on sentora server 
+BACKUPSERVERPATHTOSSHKEY="/home/backup/script/.ssh/sentora-disk"
+
+# ssh port on backup server
+BACKUPSERVERSSHPORT="22"
+    
+
+
+
+		rm -r $FOLDERBACKUP/mysql/*
+
+
+		echo "###################################" >> $FOLDERBACKUP/sentoraFullBackup.log
+		echo "###################################" >> $FOLDERBACKUP/sentoraFullBackup.log
+		echo "****** Start full backup  $(date)" >> $FOLDERBACKUP/sentoraFullBackup.log
+
+		# Start backup
+		xtrabackup --backup --datadir=/var/lib/mysql/ --target-dir=$FOLDERBACKUP/mysql/ --user=root --password=$ROOTPASSWORDFORMYSQL >> $FOLDERBACKUP/sentoraFullBackup.log
+
+		echo "" >> $FOLDERBACKUP/sentoraFullBackup.log
+		echo "****** End full backup $(date)" >> $FOLDERBACKUP/sentoraFullBackup.log
+		echo "" >> $FOLDERBACKUP/sentoraFullBackup.log
+
+
+		# start tar
+		echo "****** Start tar backup  $(date)" >> $FOLDERBACKUP/sentoraFullBackup.log
+
+		cd $FOLDERBACKUP/
+		tar -zcf sentoraFullBackup_$DAY.tar.gz mysql/ /etc/sentora/ /var/sentora/ /root/passwords.txt
+
+
+		echo "" >> $FOLDERBACKUP/sentoraFullBackup.log
+		echo "****** End tar backup  $(date)" >> $FOLDERBACKUP/sentoraFullBackup.log
+		echo "" >> $FOLDERBACKUP/sentoraFullBackup.log
+
+		# start copy
+		echo "****** Start copy backup  $(date)" >> $FOLDERBACKUP/sentoraFullBackup.log
+
+		scp -P $BACKUPSERVERSSHPORT -i $BACKUPSERVERPATHTOSSHKEY  $FOLDERBACKUP/sentoraFullBackup_$DAY.tar.gz $BACKUPSERVERUSER@$BACKUPSERVERIP:$BACKUPSERVERFOLDER/
+
+		rm $FOLDERBACKUP/sentoraFullBackup_$DAY.tar.gz
+
+		echo "" >> $FOLDERBACKUP/sentoraFullBackup.log
+		echo "****** End copy backup  $(date)" >> $FOLDERBACKUP/sentoraFullBackup.log
+		echo "" >> $FOLDERBACKUP/sentoraFullBackup.log
+		echo "" >> $FOLDERBACKUP/sentoraFullBackup.log
+		echo "" >> $FOLDERBACKUP/sentoraFullBackup.log
+		echo "" >> $FOLDERBACKUP/sentoraFullBackup.log
+		echo "" >> $FOLDERBACKUP/sentoraFullBackup.log
